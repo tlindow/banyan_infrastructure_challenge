2cdf05bb326ac76b77d94fc9f0646897db112b11
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-20 16:10:11 -0700

    Add row div around every 3 dog images

--- client/src/components/DogImagesList.jsx
+++ client/src/components/DogImagesList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import DogImage from './DogImage.jsx'
+import '../styles/DogImagesList.css'
 
 const DogImagesList = props => (
   <div className="dog-images-list">
-    {props.dogImages.map((imageURL, idx) => (
-      <DogImage key={`${idx}`} imageURL={imageURL}></DogImage>
-    ))}
+    {props.dogImages.map((imageURL, idx) => {
+      if (idx % 3 === 0) {
+        return (
+          <div className="dog-images-list-row">
+            <DogImage key={idx} imageURL={props.dogImages[idx]}></DogImage>
+            <DogImage key={idx + 1} imageURL={props.dogImages[idx + 1]}></DogImage>
+            <DogImage key={idx + 2} imageURL={props.dogImages[idx + 2]}></DogImage>
+          </div>
+        )
+      }
+    })}
   </div>
 );
 
