6b4661600b3f460eacb0291ba9f16ddd849c2974
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-20 13:21:47 -0700

    Pass down handleButtonClick method as prop

--- client/src/components/BreedList.jsx
+++ client/src/components/BreedList.jsx
@@ -4,7 +4,9 @@ import BreedButton from './BreedButton.jsx';
 const BreedList = props => (
   <div className="breed-list">
     {props.breedList.map((breed, idx) => (
-      <BreedButton key={`${breed}_${idx}`} breed={breed}></BreedButton>
+      <BreedButton key={`${breed}_${idx}`} 
+                   breed={breed}
+                   handleButtonClick={props.handleButtonClick}></BreedButton>
     ))}
   </div>
 );
