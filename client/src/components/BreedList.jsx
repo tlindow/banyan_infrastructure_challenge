bfb8aca30cd2a5675e3f6e4709865ef6b16d3e39
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-20 14:26:53 -0700

    Add lastClicked state to button if breed matches

--- client/src/components/BreedList.jsx
+++ client/src/components/BreedList.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import BreedButton from './BreedButton.jsx';
 
-const BreedList = props => (
+const BreedList = (props) => (
   <div className="breed-list">
-    {props.breedList.map((breed, idx) => (
-      <BreedButton key={`${breed}_${idx}`} 
-                   breed={breed}
-                   handleButtonClick={props.handleButtonClick}></BreedButton>
-    ))}
+    {props.breedList.map((breed, idx) => {
+      let lastClicked;
+      if (props.lastClicked === breed) {
+        lastClicked = "last-clicked";
+      } else {
+        lastClicked = "";
+      }
+      return (<BreedButton key={`${breed}_${idx}`} 
+                  breed={breed}
+                  handleButtonClick={props.handleButtonClick}
+                  lastclicked={lastClicked}></BreedButton>)
+    })}
   </div>
-);
+)
+ 
 
 export default BreedList;
