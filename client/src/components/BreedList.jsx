8d1a1680f152b6ea1cfef2fcc61db4cebb12672d
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-20 20:47:46 -0700

    Simplified BreedList rendering & add 'no results'

--- client/src/components/BreedList.jsx
+++ client/src/components/BreedList.jsx
@@ -2,46 +2,25 @@ import React from 'react';
 import BreedButton from './BreedButton.jsx';
 import '../styles/breedList.css'
 
-let lastClicked = [];
+
 const BreedList = (props) => (
+  props.breedList.length ?
   <div className="breed-list">
     {props.breedList.map((breed, idx) => {    
-      if (idx % 4 === 0) {
-        for (let i = 0; i < 4; i++) {
-          if (props.lastClicked === props.breedList[idx + i]) {
-            lastClicked.push('last-clicked');
-          } else {
-            lastClicked.push('');
-          }
+      let lastClicked = '';
+        if (props.lastClicked === breed) {
+          lastClicked = 'last-clicked';
         }
-
-        let clicked = lastClicked;
-        lastClicked = [];
-        
         return (
-          <div className="breed-list-row">
-            <BreedButton key={`${props.breedList[idx]}_${idx}`} 
-            breed={props.breedList[idx]}
-            handleButtonClick={props.handleButtonClick}
-            lastclicked={clicked[0]}></BreedButton>
-            <BreedButton key={`${props.breedList[idx + 1]}_${idx + 1}`} 
-            breed={props.breedList[idx + 1]}
-            handleButtonClick={props.handleButtonClick}
-            lastclicked={clicked[1]}></BreedButton>
-            <BreedButton key={`${props.breedList[idx + 2]}_${idx + 2}`} 
-            breed={props.breedList[idx + 2]}
-            handleButtonClick={props.handleButtonClick}
-            lastclicked={clicked[2]}></BreedButton>
-            <BreedButton key={`${props.breedList[idx + 3]}_${idx + 3}`} 
-            breed={props.breedList[idx + 3]}
-            handleButtonClick={props.handleButtonClick}
-            lastclicked={clicked[3]}></BreedButton>
-          </div>
+          <BreedButton key={props.breed} 
+          breed={breed}
+          handleButtonClick={props.handleButtonClick}
+          lastclicked={lastClicked}></BreedButton>
         )
       }
-      
-    })}
-  </div>
+    )}
+  </div> :
+  <h6 className="no-results">No search results found</h6>
 )
  
 
