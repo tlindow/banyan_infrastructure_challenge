69a91272e6b301952080a6ac6de312391e9bb56b
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-20 17:17:54 -0700

    Add materialize styling to button

--- client/src/components/BreedButton.jsx
+++ client/src/components/BreedButton.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const BreedButton = (props) => {
-  return (
-    <button onClick={() => {
-      props.handleButtonClick(props.breed);
-    }}
-            className={props.lastclicked.toString()}>{props.breed}
-    </button>
+const BreedButton = (props) => (
+      <button onClick={() => props.handleButtonClick(props.breed)}
+          className={`${props.lastclicked} waves-effect waves-light btn`}>{props.breed}
+      </button>
   )
-}
 
 export default BreedButton;
