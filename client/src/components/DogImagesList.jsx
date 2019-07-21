b065eba1339e6b2bbf3370c9ef8f51c7337d51a6
Tyler Lindow <tyler.lindow@gmail.com>
2019-07-21 11:09:59 -0700

    Simplified flex grid of images to wrap by row

--- client/src/components/DogImagesList.jsx
+++ client/src/components/DogImagesList.jsx
@@ -4,17 +4,9 @@ import '../styles/DogImagesList.css'
 
 const DogImagesList = props => (
   <div className="dog-images-list">
-    {props.dogImages.map((imageURL, idx) => {
-      if (idx % 3 === 0) {
-        return (
-          <div className="dog-images-list-row">
-            <DogImage key={idx} imageURL={props.dogImages[idx]}></DogImage>
-            <DogImage key={idx + 1} imageURL={props.dogImages[idx + 1]}></DogImage>
-            <DogImage key={idx + 2} imageURL={props.dogImages[idx + 2]}></DogImage>
-          </div>
-        )
-      }
-    })}
+    {props.dogImages.map((imageURL, idx) => (
+      <DogImage key={idx} imageURL={imageURL}></DogImage>
+    ))}
   </div>
 );
 
